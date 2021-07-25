4bcb1281784cefe0a5f842e2878fa368879205ba
Bruna Drago <bruna_drago@hotmail.com>
2021-07-25 19:50:05 -0300

    [FVC]refactoring launchScreen

--- FollowersViewCode/Resources/R.generated.swift
+++ FollowersViewCode/Resources/R.generated.swift
@@ -692,6 +692,7 @@ struct _R: Rswift.Validatable {
       let name = "LaunchScreen"
 
       static func validate() throws {
+        if UIKit.UIImage(named: "gh-logo", in: R.hostingBundle, compatibleWith: nil) == nil { throw Rswift.ValidationError(description: "[R.swift] Image named 'gh-logo' is used in storyboard 'LaunchScreen', but couldn't be loaded.") }
         if #available(iOS 11.0, tvOS 11.0, *) {
         }
       }
