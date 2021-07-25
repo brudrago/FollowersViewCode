fbea4a62cb6a9b0d017c18296dd82f8afdcec113
Bruna Drago <bruna_drago@hotmail.com>
2021-07-25 18:24:17 -0300

    [FVC]refactoring tabBar

--- FollowersViewCode/SupportFiles/SceneDelegate.swift
+++ FollowersViewCode/SupportFiles/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: windowScene)
         window?.makeKeyAndVisible()
-        window?.rootViewController = FVCTabBarViewController()
+        window?.rootViewController = FVCTabBar()
         
         configureNavigationBar()
     }
