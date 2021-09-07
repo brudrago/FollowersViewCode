8c5dc4c51a02b7e4f89ffa0e7d7fb74b9e49cb7d
Bruna Drago <bruna_drago@hotmail.com>
2021-09-07 15:00:59 -0300

    [FVC]add FollowerListTests

--- FollowersViewCodeTests/FollowerListDetails/FollowerListTests.swift
+++ FollowersViewCodeTests/FollowerListDetails/FollowerListTests.swift
@@ -9,6 +9,22 @@ import XCTest
 @testable import FollowersViewCode
 
 class FollowerListTests: XCTestCase {
+    
+    // MARK: - Public Properties
+    
+    var followerWorker: FollowerWorkerProtocol!
+    
+    var userWorker: UserInfoWorkerProtocol!
+    
+    var persistenceWorker: PersistenceWorkerProtocol!
+    
+    // MARK: - Test Lifecycle
+    
+    override func setUp() {
+        self.followerWorker = FollowerWorkerMock()
+        self.userWorker = UserWorkerMock()
+        self.persistenceWorker = PersistenceWorkerMock()
+    }
 
     // MARK: - Unit Tests
 
@@ -16,16 +32,61 @@ class FollowerListTests: XCTestCase {
         let interactor = FollowerListInteractor()
         XCTAssertNotNil(interactor)
     }
-    #warning("add workers mock")
-//    func fetchFollowers()
-//
-//    func fetchNextPage()
-//
-//    func fetchFilteredFollowers(_ filter: String)
-//
-//    func cleanAndUpdateListResultsAfterSearch()
-//
-//    func updateListFollowers()
-//
-//    func addFollowerInFavoriteList()
+    
+    func testFetchFollowers() {
+        let viewController = build()
+        viewController.interactor.fetchFollowers()
+        XCTAssertTrue(viewController.showLoadingCalled)
+    }
+    
+    func testFetchNextPage() {
+        let viewController = build()
+        viewController.interactor.fetchNextPage()
+    }
+    
+    func testFetchFilteredFollowers() {
+        let viewController = build()
+        let filter = "brudrago"
+        viewController.interactor.fetchFilteredFollowers(filter)
+        XCTAssertTrue(viewController.setCalled)
+    }
+    
+    func testCleanAndUpdateListResultsAfterSearch() {
+        let viewController = build()
+        viewController.interactor.cleanAndUpdateListResultsAfterSearch()
+        XCTAssertTrue(viewController.setCalled)
+    }
+    
+    func testUpdateListFollowers() {
+        let viewController = build()
+        viewController.interactor.updateListFollowers()
+        XCTAssertTrue(viewController.setCalled)
+    }
+    
+    func testAddFollowerInFavoriteList() {
+        let viewController = build()
+        viewController.interactor.addFollowerInFavoriteList()
+        XCTAssertTrue(viewController.showLoadingCalled)
+        XCTAssertTrue(viewController.dismissLoadingCalled)
+    }
+    
+    // MARK: - Private Functions
+    
+    private func build() -> FollowerListViewControllerMock {
+        let interactor = FollowerListInteractor(
+            followerWorker: followerWorker,
+            userWorker: userWorker,
+            persistenceWorker: persistenceWorker)
+        
+        let presenter = FollowerListPresenter()
+        let viewController = FollowerListViewControllerMock()
+        
+        viewController.interactor = interactor
+        interactor.presenter = presenter
+        presenter.viewController = viewController
+        
+        interactor.username = "brudrago"
+        
+        return viewController
+    }
 }
